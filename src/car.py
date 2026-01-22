2384b688c2f235acf609839ae8c4e73cbc898b37
Anton <farafonov-antosha@mail.ru>
2026-01-22 18:35:00 +0300

    Ruff mistakes has been fixed

--- src/car.py
+++ src/car.py
@@ -1,3 +1,11 @@
+class TooMuchFuelError(Exception):
+    pass
+
+
+class NotEnoughFuelError(Exception):
+    pass
+
+
 class Car:
     def __init__(self, model: str, fuel_capacity: float) -> None:
         self._model = model
@@ -9,15 +17,14 @@ class Car:
 
     def refuel_car(self, fuel_quantity: float):
         if self._max_fuel_capacity - self._fuel_in_tank < fuel_quantity:
-            raise Exception("Вы пытаетесь залить слишком много бензина!")
+            raise TooMuchFuelError
         self._fuel_in_tank += fuel_quantity
 
     def drive(self, distance_km: float):
         # Считаем, что расход 8 литров на 100 км
         fuel_burned: int = 8 * (distance_km / 100)
-        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11
+        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11  # noqa: FIX002
         if self._fuel_in_tank < fuel_burned:
-            raise Exception("Не доедем жеж...")
+            raise NotEnoughFuelError
         self._fuel_in_tank -= fuel_burned
         return self.get_current_fuel_level()
-#Test change
